2080ced6029c05d907000ff4bf6a239737cb5581
happy <happy@buaa.edu.cn>
2018-07-02 10:07:57 +0800

    2018-7-2

--- app/static/js/primary_info.js
+++ app/static/js/primary_info.js
@@ -8,13 +8,25 @@ function onload(){
 			$("#name").html(data[0].name);
 			$("#organization").html(data[0].organization);
 			if(data[0].email!='')
-				$("#inputEmail3").val(data[0].email);
+				$("#inputEmail3").html(data[0].email);
 			else
-				$("#inputEmail3").val("无");
+				$("#inputEmail3").html("无");
 			if(!data[0].TEL)
 				$("#inputTEL").html(data[0].TEL);
 			else
 				$("#inputTEL").html("无");
 		}
 	})
+}
+function primary_info(){
+	window.location.href="/primary_info";
+}
+function conference_manage(){
+	window.location.href="/conference_manage";
+}
+function money(){
+	window.location.href="";
+}
+function sons_manage(){
+	window.location.href="/sons_manage";
 }
